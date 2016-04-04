3d10ed3acde80b71428681402cc813fdc31e1d14
Dylan Slade <dylan.slade@icloud.com>
2016-04-04 15:06:55 -0600

    timer started

--- Challenger/TimerController.swift
+++ Challenger/TimerController.swift
@@ -12,6 +12,77 @@ class TimerController {
     // What will this class dp?
     // What we will do is manage the timer.
     // We will call functions in this class from actions like challengeAcceptedButtonTapped() and when the timer reaches 00:00:00
+    static let notificationSecondTick = "TimerNotificationSecondTick"
+    static let notificationComplete = "TimerNotificationComplete"
     
+    private(set) var seconds = NSTimeInterval(0)
+    private(set) var totalSeconds = NSTimeInterval(0)
+    private var timer: NSTimer?
+    var isOn: Bool {
+        get {
+            if timer != nil {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    var string: String {
+        get {
+            let totalSeconds = Int(self.seconds)
+            
+            let hours = totalSeconds / 3600
+            let minutes = (totalSeconds - (hours * 3600)) / 60
+            let seconds = totalSeconds - (hours * 3600) - (minutes * 60)
+            
+            var hoursString = ""
+            if hours > 0 {
+                hoursString = "\(hours):"
+            }
+            
+            var minutesString = ""
+            if minutes < 10 {
+                minutesString = "0\(minutes):"
+            } else {
+                minutesString = "\(minutes):"
+            }
+            
+            var secondsString = ""
+            if seconds < 10 {
+                secondsString = "0\(seconds)"
+            } else {
+                secondsString = "\(seconds)"
+            }
+            
+            return hoursString + minutesString + secondsString
+        }
+    }
+    
+    func setTimer(seconds: NSTimeInterval, totalSeconds: NSTimeInterval) {
+        self.seconds = seconds
+        self.totalSeconds = totalSeconds
+    }
+    
+    func startTimer() {
+        if !isOn {
+            timer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(1.0), target: self, selector: "secondTick", userInfo: nil, repeats: true)
+        }
+    }
+    
+    func stopTimer() {
+        if isOn {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    func secondTick() {
+        seconds -= 1
+        NSNotificationCenter.defaultCenter().postNotificationName(TimerController.notificationSecondTick, object: self)
+        if seconds <= 0 {
+            stopTimer()
+            NSNotificationCenter.defaultCenter().postNotificationName(TimerController.notificationComplete, object: self)
+        }
+    }
     
 }
